b6b81c5aeceb0195287c4e43a4d4deac8c487b98
Rick Sanchez <31999029+Dimension-1729@users.noreply.github.com>
2024-08-23 12:18:50 +0000

    Further discussion of object methods.

--- Basics/objects2.js
+++ Basics/objects2.js
@@ -28,3 +28,55 @@ let region = {
 }
 
 console.log(region.country.India);
+
+//The optional chaining (?.) operator accesses an object's property or calls a function. If the object accessed or function called using this operator is undefined or null, the expression short circuits and evaluates to undefined instead of throwing an error.
+
+
+console.log(region.country.Africa?.India);
+
+//Method to merge two objects
+const animal = {1: "Tiger", 2: "Lion", 3: "Rhino"}
+const birds = {4: "Peacock", 5: "Sparrow", 6: "Owl"}
+const reptiles = {1: "Snakes", 2: "Lizzard", 3: "Allegator"}
+
+const allSpecies = Object.assign(animal, birds);    //why is this not working? >> Because keys should be unique.
+
+console.log(allSpecies);
+
+//Ye saari values animal me jayengi.
+
+//Old array type method, but here each entry is an object
+
+object1 = {animal, birds}
+console.log(object1);
+
+//Using Spread operator to combine 2 or more objects
+
+object2 = {...animal,...devapp}
+console.log(object2);
+
+//How data comes from database, multiple objects inside array separated by commas; e.g:
+
+mongo_db_id246 = [
+    {key1: "Value1"},
+    {key2: "Value1"},
+    {key3: "Value13"},
+    {key4: "Value135"},
+    {key5: "Value535"},
+]
+
+console.log(mongo_db_id246[2]);
+
+//To extract keys
+console.log(Object.keys(mongo_db_id246));
+
+//To extract only values
+console.log(Object.values(devapp));
+
+//Each key value pair is turned into an array
+console.log(Object.entries(mongo_db_id246));
+
+//To query an object we use Object.hasOwnProperty(value)
+
+console.log(devapp.hasOwnProperty('isVer4')); //string should be in inverted commas
+
