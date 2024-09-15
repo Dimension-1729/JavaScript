f6de246afb0fa0751390e1d6688072190aab6917
Rick Sanchez <31999029+Dimension-1729@users.noreply.github.com>
2024-09-15 17:58:37 +0000

    Break and continue

--- JavaScript/Iterations/forloop.js
+++ JavaScript/Iterations/forloop.js
@@ -27,4 +27,51 @@ for (let i = 0; i <= 10; i++) {
     
 }
 
-//2 Table 
+//Print 2 table using for loop
+for (let i = 1; i <= 10; i++) {
+    console.log(`2 * ${i} = `,2*i);
+    ;
+    
+}
+
+//Printing all tables upto 20 using for loop
+for (let i = 1; i <=20; i++) {
+    for (let j = 1; j <=10; j++) {
+        console.log(`${i} * ${j} =`, i*j);
+        
+    };
+    
+}
+
+//for loop on array elements
+
+let myArr = ['Why', 'You', 'Are', ['Here','and'],'okay']
+
+//if I use <= array length then it will go outside the array, and give undefined as one more element
+for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+    console.log(element);
+    
+    
+}
+
+//break & continue[Ek baar condition skip kar deta hai] keywords if some specific condition arises, we can stop the loop
+
+for (let index = 0; index <= 10; index++) {
+    if (index == 7) {
+        console.log('7 detected, exitting...');
+        break
+    };
+    console.log(`The number is ${index}`);
+    
+}
+
+//continue method
+for (let index = 20; index <=45; index++) {
+    if (index == 31) {
+        console.log(`Item at index is ${index}`);
+        continue
+    }
+    console.log(`Index number: ${index}`);
+    
+}
